ca1d8c9ef1bb59ef991163b804411281596c82f4
Nick McMahon <nicholas.mcmahon@digital.justice.gov.uk>
2020-07-14 10:45:59 +0100

    Update NotificationType.kt

--- src/main/java/uk/gov/justice/digital/hmpps/cmd/api/model/NotificationType.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/cmd/api/model/NotificationType.kt
@@ -1,6 +1,6 @@
 package uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.model
 
 enum class NotificationType(val value: String) {
-    EMAIL_SUMMARY("11111111-1111-1111-111-111111111111"),
+    EMAIL_SUMMARY("11111111-1111-1111-1111-111111111111"),
     SMS_SUMMARY("22222222-2222-2222-2222-222222222222")
 }
