dc0b898136c5a5e2e97f5c53d95e87cf6c61e4fc
nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>
2020-07-16 10:29:48 +0100

    Add ControllerAdvice I removed, no need to take them out.

--- src/main/java/uk/gov/justice/digital/hmpps/cmd/api/controllers/ControllerAdvice.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/cmd/api/controllers/ControllerAdvice.kt
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.validation.FieldError
 import org.springframework.validation.ObjectError
 import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.client.RestClientException
@@ -33,6 +34,14 @@ class ControllerAdvice {
                 .body(ErrorResponse(status = (HttpStatus.INTERNAL_SERVER_ERROR.value()), developerMessage = (e.message)))
     }
 
+    @ExceptionHandler(AccessDeniedException::class)
+    fun handleAccessDeniedException(e: AccessDeniedException?): ResponseEntity<ErrorResponse> {
+        log.debug("Forbidden (403) returned", e)
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body(ErrorResponse(status = (HttpStatus.FORBIDDEN.value())))
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException::class)
     fun handleValidationExceptions(e: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
         val errors: MutableMap<String, String> = HashMap()
@@ -44,6 +53,14 @@ class ControllerAdvice {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponse(status = (HttpStatus.INTERNAL_SERVER_ERROR.value()), developerMessage = (errors.toString())))
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException::class)
+    fun handleValidationException(e: MissingServletRequestParameterException): ResponseEntity<ErrorResponse> {
+        log.debug("Bad Request (400) returned", e)
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorResponse(status = (HttpStatus.BAD_REQUEST.value()), developerMessage = (e.message)))
     }
 
     @ExceptionHandler(Exception::class)
