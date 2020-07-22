3fddc014b8c4388817a6972847380085d7d50e07
ash <aisha.nasir@digital.justice.gov.uk>
2020-07-22 10:55:43 +0100

    🎨✅ add Prison repository and tests

---
+++ src/main/java/uk/gov/justice/digital/hmpps/cmd/api/repository/PrisonRepository.kt
@@ -0,0 +1,10 @@
+package uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.repository
+
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.model.Prison
+import java.util.*
+
+@Repository
+interface PrisonRepository : CrudRepository<Prison, UUID> {
+}
---
+++ src/test/java/uk/gov/justice/digital/hmpps/cmd/api/repository/PrisonRepositoryTest.kt
@@ -0,0 +1,50 @@
+package uk.gov.justice.digital.hmpps.cmd.api.repository
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.model.Prison
+import uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.repository.PrisonRepository
+import java.time.LocalDate
+
+@ActiveProfiles("test")
+@DataJpaTest
+class PrisonRepositoryTest(
+        @Autowired val repository: PrisonRepository
+) {
+
+    private val now: LocalDate = LocalDate.now()
+
+    @BeforeEach
+    fun resetAllMocks() {
+        repository.deleteAll()
+    }
+
+    @Nested
+    @DisplayName("Get all Prisons")
+    inner class GetPreferenceTests {
+
+        @Test
+        fun `Should return all prisons`() {
+
+            val prison1 = Prison(1, "Arkham Asylum", "High security", "arkham.goth.us")
+            val prison2 = Prison(2, "The Pit", "Low security", "pit.goth.us")
+
+            val allPrisons1 = repository.findAll()
+            assertThat(allPrisons1).isNullOrEmpty()
+
+            repository.save(prison1)
+            repository.save(prison2)
+
+            val prisons2 = repository.findAll()
+            assertThat(prisons2).isNotNull
+            assertThat(prisons2).contains(prison1)
+            assertThat(prisons2).contains(prison2)
+        }
+    }
+}
