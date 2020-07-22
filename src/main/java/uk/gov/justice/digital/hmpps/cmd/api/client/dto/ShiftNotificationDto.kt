9a8a35b01f0405aea34098aa2f8a12ec8f7a2f77
nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>
2020-07-22 13:49:36 +0100

    Add a stub rest client to help with writing the service code.

---
+++ src/main/java/uk/gov/justice/digital/hmpps/cmd/api/client/dto/ShiftNotificationDto.kt
@@ -0,0 +1,38 @@
+package uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.client.dto
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import io.swagger.annotations.ApiModel
+import java.time.LocalDateTime
+
+@ApiModel(description = "CSR Notification")
+data class ShiftNotificationDto @JsonCreator constructor(
+
+        var id: Long? = null,
+
+
+        var quantumId: String,
+
+
+        var shiftDate: LocalDateTime,
+
+
+        var shiftModified: LocalDateTime,
+
+
+        var taskStart: Long?,
+
+
+        var taskEnd: Long?,
+
+
+        var task: String?,
+
+
+        var shiftType: String,
+
+
+        var actionType: String,
+
+
+        var processed: Boolean
+)
