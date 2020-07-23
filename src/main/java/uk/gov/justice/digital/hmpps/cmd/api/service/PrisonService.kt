bd08b9ce69637588e5be44532201119742256857
ash <aisha.nasir@digital.justice.gov.uk>
2020-07-23 13:18:04 +0100

    🔊 add logs for PrisonService

--- src/main/java/uk/gov/justice/digital/hmpps/cmd/api/service/PrisonService.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/cmd/api/service/PrisonService.kt
@@ -1,7 +1,9 @@
 package uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.service
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.cmd.api.service.UserPreferenceService
 import uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.model.Prison
 import uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.repository.PrisonRepository
 
@@ -11,6 +13,13 @@ import uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api
 class PrisonService(val repository: PrisonRepository) {
 
     fun getAllPrisons(): Iterable<Prison> {
-        return repository.findAll()
+        log.debug("Fetching prisons")
+        val prisons = repository.findAll()
+        log.info("Found prisons")
+        return prisons
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(PrisonService::class.java)
     }
 }
