34144e2658f87c6c0dc22aa7b471c336f8687cb5
ash <aisha.nasir@digital.justice.gov.uk>
2020-07-23 12:49:38 +0100

    🔥 remove unused imports

--- src/test/java/uk/gov/justice/digital/hmpps/cmd/api/service/PrisonServiceTest.kt
+++ src/test/java/uk/gov/justice/digital/hmpps/cmd/api/service/PrisonServiceTest.kt
@@ -8,19 +8,9 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import uk.gov.justice.digital.hmpps.cmd.api.model.ShiftNotification
-import uk.gov.justice.digital.hmpps.cmd.api.model.UserPreference
-import uk.gov.justice.digital.hmpps.cmd.api.repository.ShiftNotificationRepository
-import uk.gov.justice.digital.hmpps.cmd.api.security.AuthenticationFacade
-import uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.domain.CommunicationPreference
-import uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.domain.ShiftActionType
-import uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.domain.ShiftNotificationType
 import uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.model.Prison
 import uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.repository.PrisonRepository
 import uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.service.PrisonService
-import uk.gov.service.notify.NotificationClient
-import java.time.*
-import java.util.*
 
 @ExtendWith(MockKExtension::class)
 @DisplayName("Prison Service tests")
