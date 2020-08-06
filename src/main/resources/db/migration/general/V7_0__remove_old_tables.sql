18af02428892dde135fcc484a64bf6f4894ffdb0
nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>
2020-08-06 15:32:32 +0100

    Add a new file based on PR feedback/

--- src/main/resources/db/migration/general/V7_0__remove_old_tables.sql
+++ src/main/resources/db/migration/general/V7_0__remove_old_tables.sql
@@ -1,15 +1,9 @@
-DROP TABLE "UserNotificationSetting" CASCADE;
+DROP TABLE IF EXISTS "UserNotificationSetting" CASCADE;
 
-DROP TABLE "ShiftNotification" CASCADE;
+DROP TABLE IF EXISTS "ShiftNotification" CASCADE;
 
-DROP TABLE "ShiftTaskNotification" CASCADE;
+DROP TABLE IF EXISTS "ShiftTaskNotification" CASCADE;
 
-DROP TABLE "Prison" CASCADE;
+DROP TABLE IF EXISTS "Prison" CASCADE;
 
-DROP TABLE "NotificationConfiguration" CASCADE;
-
-UPDATE shift_notification SET shift_type = 'shift' WHERE shift_type = 'SHIFT';
-
-UPDATE shift_notification SET shift_type = 'task' WHERE shift_type = 'TASK';
-
-UPDATE shift_notification SET action_type = 'edit' WHERE action_type = 'EDIT';
+DROP TABLE IF EXISTS "NotificationConfiguration" CASCADE;
