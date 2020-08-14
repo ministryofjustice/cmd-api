b3c1b4d5c164e0fdc453d28f867fc9106c5ac661
Nick McMahon <nicholas.mcmahon@digital.justice.gov.uk>
2020-08-14 09:17:23 +0100

    Force to Upper checks for quantumId (#45)
    Prison-officer-diary in prod is not at latest - its sending task and not shift/overtime
    
    Co-authored-by: nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>

--- src/main/java/uk/gov/justice/digital/hmpps/cmd/api/service/UserPreferenceService.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/cmd/api/service/UserPreferenceService.kt
@@ -35,7 +35,7 @@ class UserPreferenceService(val repository: UserPreferenceRepository, val authen
     }
 
     fun getOrCreateUserPreference(quantumId: String = authenticationFacade.currentUsername): UserPreference {
-        val userPreference = repository.findByQuantumId(quantumId)
+        val userPreference = repository.findByQuantumId(quantumId.toUpperCase())
         log.info("Finding user preference for $quantumId")
         return if (userPreference != null) {
             log.info("Found user preference for $quantumId")
@@ -43,7 +43,7 @@ class UserPreferenceService(val repository: UserPreferenceRepository, val authen
             userPreference
         } else {
             log.info("Creating new user preference for $quantumId")
-            repository.save(UserPreference(quantumId))
+            repository.save(UserPreference(quantumId.toUpperCase()))
         }
     }
 
