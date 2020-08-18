6073743fdf672e6b022e14fdabc9264d4c0e8cfe
Nick McMahon <nicholas.mcmahon@digital.justice.gov.uk>
2020-08-18 20:18:25 +0100

    Nm remove transactions (#56)
    * Be more granular around marking notifications as processed.
    + use Hikari default values.
    
    * Remove unneeded transactions.
    
    * Remove unneeded transactions.
    
    Co-authored-by: nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>

--- src/main/java/uk/gov/justice/digital/hmpps/cmd/api/service/NotificationService.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/cmd/api/service/NotificationService.kt
@@ -172,6 +172,7 @@ class NotificationService(
         if (processOnRead) {
             notifications.forEach { it.processed = true }
         }
+        shiftNotificationRepository.saveAll(notifications)
         return notificationDtos.distinct()
     }
 
