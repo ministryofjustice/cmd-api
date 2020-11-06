5be26ed65dad0632876c098589ad020c5f55c8fb
nickmcmahon01 <nick.mcmahon@digital.justice.gov.uk>
2020-11-06 14:29:42 +0000

    🔧 Tidyup unused tables and and keep on top of out-of-date notification data.

--- src/main/java/uk/gov/justice/digital/hmpps/cmd/api/config/QuartzConfiguration.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/cmd/api/config/QuartzConfiguration.kt
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.core.io.ClassPathResource
 import org.springframework.scheduling.quartz.CronTriggerFactoryBean
 import org.springframework.scheduling.quartz.JobDetailFactoryBean
