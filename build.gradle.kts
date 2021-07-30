f4a16a8a5e758b877550ff204a46a00f007e631f
Connor Glynn <connor.glynn@digital.justice.gov.uk>
2021-07-30 16:08:43 +0100

    🔒PAT-1990 cmd-api failed hmpps/trivy_latest_scan

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.4"
-  kotlin("plugin.spring") version "1.5.20"
-  kotlin("plugin.jpa") version "1.5.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.5"
+  kotlin("plugin.spring") version "1.5.21"
+  kotlin("plugin.jpa") version "1.5.21"
 }
 
 repositories {
@@ -18,7 +18,7 @@ dependencies {
 
   runtimeOnly("com.h2database:h2")
   runtimeOnly("org.postgresql:postgresql")
-  runtimeOnly("com.zaxxer:HikariCP:3.4.5")
+  runtimeOnly("com.zaxxer:HikariCP:5.0.0")
   runtimeOnly("org.flywaydb:flyway-core")
 
   implementation("org.springframework.boot:spring-boot-starter-jdbc")
@@ -33,13 +33,13 @@ dependencies {
   implementation("org.springframework:spring-webflux")
   implementation("org.springframework.boot:spring-boot-starter-reactor-netty")
 
-  implementation("io.springfox:springfox-swagger2:2.9.2")
-  implementation("io.springfox:springfox-swagger-ui:2.9.2")
-  implementation("org.apache.commons:commons-lang3:3.11")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.3")
+  implementation("io.springfox:springfox-swagger2:3.0.0")
+  implementation("io.springfox:springfox-swagger-ui:3.0.0")
+  implementation("org.apache.commons:commons-lang3:3.12.0")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.4")
 
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.3")
-  implementation("com.nimbusds:nimbus-jose-jwt:9.10.1")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.4")
+  implementation("com.nimbusds:nimbus-jose-jwt:9.11.1")
   implementation("com.google.guava:guava:30.1.1-jre")
   implementation("uk.gov.service.notify:notifications-java-client:3.17.2-RELEASE")
 
@@ -51,9 +51,9 @@ dependencies {
   }
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("com.tngtech.java:junit-dataprovider:1.13.1")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.19.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.27.0")
   testImplementation("io.github.http-builder-ng:http-builder-ng-apache:1.0.4")
-  testImplementation("com.ninja-squad:springmockk:2.0.3")
+  testImplementation("com.ninja-squad:springmockk:3.0.1")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
 }
 
