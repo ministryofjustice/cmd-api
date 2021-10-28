87103a297ab6631c8c9708726257570da97f9c77
steverendell <steve.rendell@syscon.net>
2021-10-28 14:50:09 +0100

    PAT-2095 Security owasp failure for spring projects

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.9"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.12"
   kotlin("plugin.spring") version "1.5.31"
   kotlin("plugin.jpa") version "1.5.31"
 }
@@ -40,10 +40,10 @@ dependencies {
   implementation("io.springfox:springfox-swagger2:2.9.2")
   implementation("io.springfox:springfox-swagger-ui:2.9.2")
   implementation("org.apache.commons:commons-lang3:3.12.0")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.5")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.5")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.0")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
   implementation("com.nimbusds:nimbus-jose-jwt:9.14")
-  implementation("com.google.guava:guava:31.0-jre")
+  implementation("com.google.guava:guava:31.0.1-jre")
   implementation("uk.gov.service.notify:notifications-java-client:3.17.2-RELEASE")
 
   testAnnotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
