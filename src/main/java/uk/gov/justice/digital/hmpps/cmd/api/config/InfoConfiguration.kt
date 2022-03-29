a9fb1e6e610ad1693b534cb7bba097816584c1e1
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2022-03-29 18:51:32 +0100

    Sdi 141 investigate and correct cache (#111)
    * SDI-141 investigate and correct cache
    
    * SDI-141 investigate and correct cache
    
    * SDI-141 investigate and correct cache

---
+++ src/main/java/uk/gov/justice/digital/hmpps/cmd/api/config/InfoConfiguration.kt
@@ -0,0 +1,23 @@
+package uk.gov.justice.digital.hmpps.cmd.api.uk.gov.justice.digital.hmpps.cmd.api.config
+
+import com.github.benmanes.caffeine.cache.Cache
+import org.springframework.boot.actuate.info.Info.Builder
+import org.springframework.boot.actuate.info.InfoContributor
+import org.springframework.cache.CacheManager
+import org.springframework.stereotype.Component
+
+/**
+ * Adds some basic stats for the /user/details cache to /info
+ */
+@Component
+class InfoConfiguration(private val cacheManager: CacheManager) : InfoContributor {
+
+  override fun contribute(builder: Builder) {
+    val cache = cacheManager.getCache("userDetails")
+    val nativeCache = cache!!.nativeCache
+    if (nativeCache is Cache<*, *>) {
+      val stats = nativeCache.stats()
+      builder.withDetail("cache-userDetails", stats.toString())
+    }
+  }
+}
