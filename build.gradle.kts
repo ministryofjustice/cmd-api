691e9b1a757b16fb9fd86b76ef425c28c3f9b559
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2022-06-01 15:43:25 +0100

    SDI-140 make polling batch job take over (#137)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.2.0-beta-4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.2.0"
   kotlin("plugin.spring") version "1.6.21"
   kotlin("plugin.jpa") version "1.6.21"
   idea
@@ -32,11 +32,10 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-cache")
-  implementation("org.springframework.boot:spring-boot-starter-quartz")
   implementation("com.github.ben-manes.caffeine:caffeine")
 
-  implementation("net.javacrumbs.shedlock:shedlock-spring:4.35.0")
-  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.35.0")
+  implementation("net.javacrumbs.shedlock:shedlock-spring:4.36.0")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:4.36.0")
 
   implementation("org.springframework:spring-webflux")
   implementation("org.springframework.boot:spring-boot-starter-reactor-netty")
