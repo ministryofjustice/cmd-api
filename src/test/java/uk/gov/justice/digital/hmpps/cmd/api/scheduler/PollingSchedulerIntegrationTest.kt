b9783ba975bf3d8e6d55e352c8c2b682e444ccfa
steverendell <steve.rendell@syscon.net>
2023-10-11 10:58:27 +0100

    SDIT-1113 Respond to Syscon security report

--- src/test/java/uk/gov/justice/digital/hmpps/cmd/api/scheduler/PollingSchedulerIntegrationTest.kt
+++ src/test/java/uk/gov/justice/digital/hmpps/cmd/api/scheduler/PollingSchedulerIntegrationTest.kt
@@ -21,7 +21,7 @@ import uk.gov.justice.digital.hmpps.prisonertonomisupdate.wiremock.PrisonApiExte
 import java.time.LocalDateTime
 
 @ExtendWith(PrisonApiExtension::class, CsrApiExtension::class, HmppsAuthApiExtension::class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = ["csr.regions=1s"])
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = ["csr.timeout=1s"])
 @ActiveProfiles(value = ["test"])
 @DisplayName("Integration Tests for Polling Scheduler")
 class PollingSchedulerIntegrationTest(
