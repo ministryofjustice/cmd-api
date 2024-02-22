5b7994463fc1db1bf0d5d3e662a6179a84c51211
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-02-22 16:57:55 +0000

    SDIT-1525: ✨ Use new kotlin lib (#249)

---
+++ src/main/java/uk/gov/justice/digital/hmpps/cmd/api/config/LockProviderConfiguration.kt
@@ -0,0 +1,28 @@
+package uk.gov.justice.digital.hmpps.cmd.api.config
+
+import net.javacrumbs.shedlock.core.LockProvider
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+class LockProviderConfiguration {
+  @Bean
+  fun lockProvider(jdbcTemplate: JdbcTemplate): LockProvider =
+    JdbcTemplateLockProvider(
+      JdbcTemplateLockProvider.Configuration.builder()
+        .withJdbcTemplate(jdbcTemplate)
+        .usingDbTime()
+        .build(),
+    )
+}
+
+@Configuration
+@Profile("!test") // prevent scheduler running during integration tests
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtLeastFor = "PT1M", defaultLockAtMostFor = "PT1H")
+class SpringSchedulingConfig
