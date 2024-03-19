4353a024a277e139976afab2dacadb1810b9a842
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-03-19 11:39:17 +0000

    SDIT-1600: 🔧 Try out latest 3.0 helm chart (#255)

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,23 +26,23 @@ dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
   runtimeOnly("com.h2database:h2:2.2.224")
-  runtimeOnly("org.postgresql:postgresql:42.7.2")
+  runtimeOnly("org.postgresql:postgresql:42.7.3")
   runtimeOnly("com.zaxxer:HikariCP")
   runtimeOnly("org.flywaydb:flyway-core")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.2.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.2.2")
   implementation("org.springframework.boot:spring-boot-starter-jdbc")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-cache")
   implementation("com.github.ben-manes.caffeine:caffeine")
 
-  implementation("net.javacrumbs.shedlock:shedlock-spring:5.11.0")
-  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:5.11.0")
+  implementation("net.javacrumbs.shedlock:shedlock-spring:5.12.0")
+  implementation("net.javacrumbs.shedlock:shedlock-provider-jdbc-template:5.12.0")
 
   implementation("org.springframework:spring-webflux")
   implementation("org.springframework.boot:spring-boot-starter-reactor-netty")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
 
   implementation("org.apache.commons:commons-lang3:3.14.0")
   implementation("uk.gov.service.notify:notifications-java-client:5.0.0-RELEASE")
@@ -60,7 +60,7 @@ dependencies {
   testImplementation("com.ninja-squad:springmockk:4.0.2")
   testImplementation("io.jsonwebtoken:jjwt-impl:0.12.5")
   testImplementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
-  testImplementation("org.wiremock:wiremock-standalone:3.4.1")
+  testImplementation("org.wiremock:wiremock-standalone:3.4.2")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
 }
 
