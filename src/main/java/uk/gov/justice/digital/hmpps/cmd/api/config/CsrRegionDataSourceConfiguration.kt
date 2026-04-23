445bf75a1eec06ef50781ea9ad3eb1f4e781eb74
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-04-23 16:45:30 +0100

    SDIT-3674: ♻️  Switch to single database connection rather than per region (#395)

--- src/main/java/uk/gov/justice/digital/hmpps/cmd/api/config/CsrRegionDataSourceConfiguration.kt
+++ src/main/java/uk/gov/justice/digital/hmpps/cmd/api/config/CsrRegionDataSourceConfiguration.kt
@@ -5,31 +5,32 @@ import org.flywaydb.core.Flyway
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.jdbc.datasource.DataSourceTransactionManager
-import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource
 import org.springframework.transaction.PlatformTransactionManager
-import uk.gov.justice.digital.hmpps.cmd.api.utils.RegionContext
 import javax.sql.DataSource
 
 @Configuration(proxyBeanMethods = false)
-@EnableConfigurationProperties
+@EnableConfigurationProperties(CsrConfiguration::class)
 class CsrRegionDataSourceConfiguration(
-  @Autowired private val regionData: Regions,
+  @Autowired private val csrConfiguration: CsrConfiguration,
 ) {
 
   @Bean(defaultCandidate = false)
-  fun regionDataSource(@Value($$"${csr.flyway.enabled:false}") flywayEnabled: Boolean): DataSource = RegionAwareRoutingSource().apply {
-    // Read the region array in from application properties and construct data sources mapped to region names
-    setTargetDataSources(
-      regionData.regions.associate { it.name to regionDataSource(it) },
-    )
+  fun regionDataSource(@Value($$"${csr.flyway.enabled:false}") flywayEnabled: Boolean): DataSource = HikariDataSource().also {
+    it.driverClassName = csrConfiguration.driverClassName
+    it.jdbcUrl = csrConfiguration.url
+    it.username = csrConfiguration.username
+    it.password = csrConfiguration.password
+    it.setReadOnly(true)
+    it.setMaximumPoolSize(20)
   }.also {
     if (flywayEnabled) {
-      migrateRegionDb(regionDataSource(regionData.regions.first()))
+      migrateRegionDb(it)
     }
   }
 
@@ -39,15 +40,6 @@ class CsrRegionDataSourceConfiguration(
   @Bean(defaultCandidate = false)
   fun regionTransactionManager(@Qualifier("regionDataSource") regionDataSource: DataSource): PlatformTransactionManager = DataSourceTransactionManager(regionDataSource)
 
-  private fun regionDataSource(region: Region): HikariDataSource = HikariDataSource().also {
-    it.driverClassName = region.driverClassName
-    it.jdbcUrl = region.url
-    it.username = region.username
-    it.password = region.password
-    it.schema = region.schema
-    it.setReadOnly(true)
-    it.setMaximumPoolSize(35)
-  }
   private fun migrateRegionDb(regionDataSource: HikariDataSource) {
     regionDataSource.setReadOnly(false)
     Flyway.configure()
@@ -60,6 +52,16 @@ class CsrRegionDataSourceConfiguration(
   }
 }
 
-class RegionAwareRoutingSource : AbstractRoutingDataSource() {
-  override fun determineCurrentLookupKey(): Any = RegionContext.getRegion().toString()
-}
+@ConfigurationProperties(prefix = "csr")
+data class CsrConfiguration(
+  val username: String,
+  val password: String,
+  val url: String,
+  val driverClassName: String,
+  val regions: List<Region>,
+)
+
+data class Region(
+  val name: Int,
+  val schema: String,
+)
