4df056abe4fd29744439a2c4f3f379c8a2aed168
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-04-29 10:35:19 +0100

    SDIT-3752: 🗃️ Add in missing prisons

---
+++ src/main/resources/db/migration/general/V16_0__add_prisons.sql
@@ -0,0 +1,3 @@
+INSERT INTO prison (prison_id, region, csr_plan_unit, prison_name)
+    VALUES ('FYI', 3, 'Feltham A', 'HMP Feltham A'),
+           ('LGI', 6, 'Lowdham Grange', 'HMP Lowdham Grange');
